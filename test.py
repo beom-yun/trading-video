31dadbfef5c15c95a0ffebd64734848fefb94fa6
beom-yun <scds0326@naver.com>
2025-02-20 15:24:35 +0900

    transactions

--- test.py
+++ test.py
@@ -3,11 +3,23 @@ from datetime import datetime
 from collections import deque
 
 
+mock_fee = 14  # 모의계좌 수수료(USD)
+
+
 def print_trades():
     print("<< trades >>")
     for k in trades:
         print(f"[종목 : {k}]")
         for r in trades[k]:
+            print(r)
+    print()
+
+
+def print_tractions():
+    print("<< transactions >>")
+    for k in transactions:
+        print(f"[종목 : {k}]")
+        for r in transactions[k]:
             print(r)
     print()
 
@@ -42,20 +54,31 @@ for i in range(len(data)):
     }
 
 # trades : 종목별 청산내역 queue
+# transactions : 매매 트랜잭션(싸이클 시작에서부터 종료까지)
 trades = dict()
+transactions = dict()
+
 while data:
     new_trade = data.popleft()
-    print(f"새로 들어온 : {new_trade}")
 
-    # 종목 자체가 없으면 새로 만들기기
+    # 종목 자체가 없으면 새로 만들기
     if new_trade["종목"] not in trades:
         trades[new_trade["종목"]] = deque()
+        transactions[new_trade["종목"]] = list()
 
     # 종목 queue가 비어있다면 새로운 거래내역을 append
+    # 매매 싸이클 시작
     if not trades[new_trade["종목"]]:
         trades[new_trade["종목"]].append(new_trade)
-        print("매매 싸이클 시작")
-        print_trades()
+        transactions[new_trade["종목"]].append(
+            {
+                "start_time": new_trade["일시"],
+                "end_time": None,
+                "type": new_trade["구분"],
+                "pnl": 0,
+                "fee": 0,
+            }
+        )
         continue
 
     # 종목 queue가 비어있지 않을 경우
@@ -68,22 +91,47 @@ while data:
         trades[new_trade["종목"]].append(new_trade)
     # 매매구분이 다를 경우 (매수 - 매도 / 매도 - 매수)
     else:
+        tr_size = min(x["수량"], new_trade["수량"])
+
         if x["수량"] == new_trade["수량"]:
-            print("청산")
+            if transactions[new_trade["종목"]][-1]["type"] == "매수":
+                transactions[new_trade["종목"]][-1]["pnl"] += (
+                    new_trade["금액"] - x["금액"]
+                )
+            else:
+                transactions[new_trade["종목"]][-1]["pnl"] += (
+                    x["금액"] - new_trade["금액"]
+                )
         elif x["수량"] > new_trade["수량"]:
-            print(f"일부청산")
+            if transactions[new_trade["종목"]][-1]["type"] == "매수":
+                transactions[new_trade["종목"]][-1]["pnl"] += (
+                    new_trade["금액"] - (x["금액"] // x["수량"]) * tr_size
+                )
+            else:
+                transactions[new_trade["종목"]][-1]["pnl"] += (
+                    x["금액"] // x["수량"]
+                ) * tr_size - new_trade["금액"]
             x["금액"] = (x["금액"] // x["수량"]) * (x["수량"] - new_trade["수량"])
             x["수량"] = x["수량"] - new_trade["수량"]
             trades[new_trade["종목"]].appendleft(x)
         else:
-            print("일부청산2")
+            if transactions[new_trade["종목"]][-1]["type"] == "매수":
+                transactions[new_trade["종목"]][-1]["pnl"] += (
+                    new_trade["금액"] // new_trade["수량"]
+                ) * tr_size - x["금액"]
+            else:
+                transactions[new_trade["종목"]][-1]["pnl"] += (
+                    new_trade["금액"] // new_trade["수량"]
+                ) * tr_size - x["금액"]
             new_trade["금액"] = (new_trade["금액"] // new_trade["수량"]) * (
                 new_trade["수량"] - x["수량"]
             )
             new_trade["수량"] = new_trade["수량"] - x["수량"]
             data.appendleft(new_trade)
+        transactions[new_trade["종목"]][-1]["fee"] += mock_fee * tr_size
 
+    # 매매 싸이클 종료
     if not trades[new_trade["종목"]]:
-        print("매매 싸이클 종료")
+        transactions[new_trade["종목"]][-1]["end_time"] = new_trade["일시"]
 
-    print_trades()
+print_tractions()
