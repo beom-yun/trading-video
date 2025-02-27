505fad432cdbc1c7c3bdb801a4df09c4ebac22e6
beom-yun <scds0326@naver.com>
2025-02-28 01:21:51 +0900

    buttons click

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5 import uic
 
 
@@ -20,7 +20,31 @@ class MainWidget(QWidget, form_class):
         self.initUi()
 
     def initUi(self):
+        # 윈도우아이콘, 로고 설정
         self.setWindowIcon(QIcon(CANDLESTICK_ICON))
+        self.lbl_logo.setPixmap(QPixmap(NEXT_SECU_ICON))
+
+        # '파일 열기', '초기화' 버튼 클릭
+        self.btn_open_file.clicked.connect(self.open_file)
+        self.btn_rst_file.clicked.connect(self.reset_file)
+
+    # '파일 열기' 버튼 클릭
+    def open_file(self):
+        video_file = QFileDialog.getOpenFileName(
+            self, caption="녹화영상 파일 선택", filter="mp4 file(*.mp4)"
+        )
+        if not video_file[0]:
+            return
+        csv_file = QFileDialog.getOpenFileName(
+            self, caption="거래내역 파일 선택", filter="csv file(*.csv)"
+        )
+        if not csv_file[0]:
+            return
+        print(video_file[0], csv_file[0])
+
+    # '초기화' 버튼 클릭
+    def reset_file(self):
+        print("녹화영상, 거래내역 삭제")
 
 
 if __name__ == "__main__":
