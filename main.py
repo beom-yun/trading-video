f10c8e0fc3bac5de80a52a53ccc44f977ef7b463
beom-yun <scds0326@naver.com>
2025-02-28 22:18:23 +0900

    mainWidget open file

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ CANDLESTICK_ICON = "img/candlestick.png"
 NEXT_SECU_ICON = "img/next-securities.png"
 ###
 
+### 상수
+DEFAULT_OFFSET = 5
+###
+
 # UI 파일 가져오기
 form_class = uic.loadUiType(MAIN_WIDGET)[0]
 dlg_video = uic.loadUiType(DIALOG_VIDEO)[0]
@@ -20,6 +24,12 @@ class DialogVideo(QDialog, dlg_video):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.buttonBox.accepted.connect(self.dialog_accepted)
+        self.video_start_time = None
+
+    def dialog_accepted(self):
+        self.video_start_time = self.dateTimeEdit.text()
+        self.close()
 
 
 class MainWidget(QWidget, form_class):
@@ -41,35 +51,56 @@ class MainWidget(QWidget, form_class):
 
     # '파일 열기' 버튼 클릭
     def open_file(self):
+        # 녹화영상 파일 체크
         video_file = QFileDialog.getOpenFileName(
             self, caption="녹화영상 파일 선택", filter="mp4 file(*.mp4)"
         )
         if not video_file[0]:
             return
+
+        res_v = self.v.open_video(video_file[0])
+
+        # 영상 시작시간 설정
+        # 파일명으로 시작시간을 알 수 없다면, dialog로 직접 입력받음
+        accepted = True
+        if res_v["datetime"]:
+            self.lbl_video_start_time.setText(res_v["datetime_str"])
+        else:
+            dlg = DialogVideo()
+            accepted = bool(dlg.exec_())
+            self.lbl_video_start_time.setText(dlg.video_start_time)
+        if not accepted:
+            return
+
+        # 파일명, 영상길이 label 입력
+        self.lbl_video_file.setText(str(res_v["file_name"]))
+        self.lbl_video_duration.setText(str(res_v["duration"]))
+        # print(res_v)
+
+        # 거래내역 파일 체크
         csv_file = QFileDialog.getOpenFileName(
             self, caption="거래내역 파일 선택", filter="csv file(*.csv)"
         )
         if not csv_file[0]:
             return
 
-        # 녹화영상 파일 체크
-        res = self.v.open_video(video_file[0])
-        # 파일명, 영상길이 label 입력
-        self.lbl_video_file.setText(str(res["file_name"]))
-        self.lbl_video_duration.setText(str(res["duration"]))
-        # 영상 시작시간 설정
-        if res["datetime"]:
-            self.edit_start_time.setDateTime(res["datetime"])
-        else:
-            DialogVideo().exec_()
-        print(res)
-        ### '분석' 버튼을 따로 만드는게 좋을 듯
-
-        # 거래내역 파일 체크
-
     # '초기화' 버튼 클릭
     def reset_file(self):
-        print("녹화영상, 거래내역 삭제")
+        self.v.reset_video()
+
+        # UI 초기화
+        self.lbl_video_file.clear()
+        self.lbl_video_duration.clear()
+        self.lbl_video_start_time.clear()
+        self.edit_offset_start.setValue(DEFAULT_OFFSET)
+        self.edit_offset_end.setValue(DEFAULT_OFFSET)
+        self.lbl_csv_file.clear()
+        self.lbl_csv_cnt.clear()
+        self.btn_all_check.setEnabled(False)
+        self.chk_text.setEnabled(False)
+        self.chk_text.setChecked(False)
+        self.list_widget.clear()
+        self.btn_make.setEnabled(False)
 
 
 if __name__ == "__main__":
--- video.py
+++ video.py
@@ -13,11 +13,18 @@ class Video:
 
         try:
             dt = datetime.strptime(name.split(".")[0], "%Y-%m-%d %H-%M-%S")
+            dt_str = dt.strftime("%Y-%m-%d %H:%M:%S")
         except:
             dt = None
+            dt_str = None
 
         return {
             "file_name": name,  # 파일명
             "duration": self.data.duration,  # 영상길이
             "datetime": dt,  # 영상 시작시간
+            "datetime_str": dt_str,  # 영상 시작시간
         }
+
+    # mainWidget에서 '초기화' 동작
+    def reset_video(self):
+        self.data = None
